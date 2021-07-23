e7758faac50be01e012fd49e014e9531aa479345
Jagmeet <jagmeetsarna2412@gmail.com>
2021-07-23 03:38:00 +0200

    Removed some unnecessary lines

--- Conv2D_prototype.py
+++ Conv2D_prototype.py
@@ -10,18 +10,18 @@ import math
 
 os.chdir(r"D:\TOPIOS Data\data\twentyone")
 
-kk = h5py.File("data_topios.h5", "r")
+particleDensity = h5py.File("data_topios.h5", "r")
 
-k=kk['dataset_1'][()]
+particleDensity = particleDensit['ParticleDensiy'][()]
 
 #shape(x_train)
 x_train=[]
 y_train=[]
-for i in range(len(k)-10): #len(k)-10
+for i in range(len(particleDensity)-10): #len(k)-10
   
 
-  x_train.append(k[i:i+10])
-  y_train.append(k[i+1:i+11])
+  x_train.append(particleDensity[i:i+10])
+  y_train.append(particleDensity[i+1:i+11])
 
 x_train = np.array(x_train)  
 y_train = np.array(y_train)
@@ -39,7 +39,7 @@ import pylab as plt
 seq = Sequential()
 
 seq.add(ConvLSTM2D(filters=40, kernel_size=(5, 5),
-                   input_shape=(None,360,720,1),
+                   input_shape=(None,360,720,1), #important thing to note
                    padding='same', return_sequences=True))
 seq.add(BatchNormalization())
 
@@ -47,8 +47,8 @@ seq.add(ConvLSTM2D(filters=40, kernel_size=(3, 3),
                    padding='same', return_sequences=True))
 seq.add(BatchNormalization())
 
-seq.add(ConvLSTM2D(filters=40, kernel_size=(3, 3),
                    padding='same', return_sequences=True))
+seq.add(ConvLSTM2D(filters=40, kernel_size=(3, 3),
 seq.add(BatchNormalization())
 
 seq.add(ConvLSTM2D(filters=40, kernel_size=(3, 3),
@@ -60,7 +60,6 @@ seq.add(Conv3D(filters=1, kernel_size=(3, 3, 3),
                padding='same', data_format='channels_last'))
 seq.compile(loss='binary_crossentropy', optimizer='adadelta')
 
-shape(x_train[0:5])
 
-seq.fit(x_arr[0:5], y_arr[0:5], batch_size=1,epochs=5, validation_split=0.05)
-print("jag")
+
+seq.fit(x_arr, y_arr, batch_size=1,epochs=5, validation_split=0.05)
